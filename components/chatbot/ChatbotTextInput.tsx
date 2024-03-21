a04446bf62df95da6193c4e15bcb946813c71852
Kennan Wu <kennan.k.wu@gmail.com>
2024-03-20 22:55:22 -0500

    change submit button styling

--- components/chatbot/ChatbotTextInput.tsx
+++ components/chatbot/ChatbotTextInput.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { View, TextInput, StyleSheet, Pressable } from "react-native";
 import { useDispatch } from "react-redux";
 import { addUserChatMessage } from "../../redux/slices/chat-slice";
-import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function ChatbotTextInput() {
   const [message, setMessage] = useState("");
@@ -18,27 +18,29 @@ export default function ChatbotTextInput() {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        editable
-        multiline
-        placeholder="What's on your mind?"
-        placeholderTextColor={"#DBC9C9"}
-        style={styles.textinput}
-        onChangeText={setMessage}
-        value={message}
-      />
-      <Pressable
-        style={styles.sendButtonContainer}
-        onPress={
-          message.replace(/\s+/g, "") === "" ? handleMicrophone : handleSubmit
-        }
-      >
-        <FontAwesome
-          name={message.replace(/\s+/g, "") === "" ? "microphone" : "send"}
-          size={25}
-          color={"white"}
+      <View style={styles.userInputContainer}>
+        <TextInput
+          editable
+          multiline
+          placeholder="What's on your mind?"
+          placeholderTextColor={"#DBC9C9"}
+          style={styles.textinput}
+          onChangeText={setMessage}
+          value={message}
         />
-      </Pressable>
+        <Pressable
+          style={styles.sendButtonContainer}
+          onPress={
+            message.replace(/\s+/g, "") === "" ? handleMicrophone : handleSubmit
+          }
+        >
+          <Ionicons
+            name={message.replace(/\s+/g, "") === "" ? "mic" : "arrow-up"}
+            size={20}
+            color={"white"}
+          />
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -49,25 +51,29 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center",
     justifyContent: "center",
-    padding: 16,
+    padding: 8,
     borderTopColor: "#A9A9A9",
     borderTopWidth: 1,
-    flexDirection: "row",
   },
-  textinput: {
-    flex: 1,
+  userInputContainer: {
+    width: "100%",
+    flexDirection: "row",
     borderWidth: 1,
     borderColor: "#A9A9A9",
     borderRadius: 10,
-    padding: 15,
     justifyContent: "center",
+    alignItems: "center",
+    padding: 8,
+  },
+  textinput: {
+    flex: 1,
     fontSize: 16,
     color: "white",
   },
   sendButtonContainer: {
-    width: 50,
-    height: 50,
-    marginLeft: 16,
+    width: 35,
+    height: 35,
+    marginLeft: 8,
     backgroundColor: "#272020",
     borderWidth: 1,
     borderRadius: 30,
