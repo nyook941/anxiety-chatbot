4bad8e745d296429816d51ff8840754fafb81352
nyook941 <kennan.k.wu@gmail.com>
2024-03-21 12:51:48 -0500

    add mood slice

---
+++ redux/slices/moodDiary-slice.ts
@@ -0,0 +1,13 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState: any = {};
+
+export const moodSlice = createSlice({
+  name: "mood",
+  initialState,
+  reducers: {},
+});
+
+export const {} = moodSlice.actions;
+
+export default moodSlice.reducer;
