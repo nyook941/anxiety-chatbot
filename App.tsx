a1ecde0831a435cde5d82654e43f2ae07405d9cb
nyook941 <kennan.k.wu@gmail.com>
2024-04-11 12:48:22 -0500

    generate user model

--- App.tsx
+++ App.tsx
@@ -8,6 +8,12 @@ import { Amplify } from "aws-amplify";
 import amplifyconfig from "./src/amplifyconfiguration.json";
 import { withAuthenticator } from "@aws-amplify/ui-react-native";
 Amplify.configure(amplifyconfig);
+import { DataStore } from "aws-amplify/datastore";
+import { SQLiteAdapter } from "@aws-amplify/datastore-storage-adapter/SQLiteAdapter";
+
+DataStore.configure({
+  storageAdapter: SQLiteAdapter,
+});
 
 const App = () => {
   return (
