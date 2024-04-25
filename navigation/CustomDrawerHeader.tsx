a0377b79201b0b84170460c8c52236d180e66951
Kennan Wu <kennan.k.wu@gmail.com>
2024-04-25 14:14:52 -0500

    begin working on log out button

---
+++ navigation/CustomDrawerHeader.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function CustomDrawerHeader() {
+  return (
+    <View>
+      <Text>CustomDrawerHeader</Text>
+    </View>
+  );
+}
